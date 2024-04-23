fc7cb8cf6921d36a5daaabdbf1f266e007e064a1
202007140012 <claudio.mello@itec.ufpa.br>
2024-04-23 02:34:25 -0300

    randomly order the h5

--- preprocess_h5.py
+++ preprocess_h5.py
@@ -15,8 +15,23 @@ OUTPUT_NAME = "melanoma.h5"  # Output file name
 
 # Get all image file names in the directory for benign
 train_benign_files = [
-    file for file in os.listdir(train_benign_directory) if file.endswith(".jpg")
+    (file, 0) for file in os.listdir(train_benign_directory) if file.endswith(".jpg")
 ]
+
+# Get all image file names in the directory for malignant
+train_malignant_files = [
+    (file, 1) for file in os.listdir(train_malignant_directory) if file.endswith(".jpg")
+]
+
+
+# Concatenate the benign and malignant file names
+train_files = train_benign_files + train_malignant_files
+
+# Shuffle the file names
+import random
+
+random.shuffle(train_files)
+
 
 # Define the transformation for rotating the images
 transform30 = transforms.Compose(
@@ -46,11 +61,12 @@ except FileNotFoundError:
 with h5py.File(OUTPUT_NAME, "x") as f:
     f.create_dataset("train", (0, 3, 224, 224), maxshape=(None, 3, 224, 224))
     f.create_dataset("train_labels", (0,), maxshape=(None,))
-    for file in tqdm.tqdm(
-        train_benign_files, desc="Processing benign images", leave=False
+    for file, label in tqdm.tqdm(
+        train_files, desc="Processing training images", leave=False
     ):
+        directory = train_benign_directory if label == 0 else train_malignant_directory
         image = torchvision.io.read_image(
-            os.path.join(train_benign_directory, file),
+            os.path.join(directory, file),
             torchvision.io.image.ImageReadMode.RGB,
         )
         image30 = transform30(image)
@@ -63,77 +79,47 @@ with h5py.File(OUTPUT_NAME, "x") as f:
         f["train"].resize(f["train"].shape[0] + 5, axis=0)
         f["train"][-5:] = torch.stack([image30, image45, image60, image90, image0])
         f["train_labels"].resize(f["train_labels"].shape[0] + 5, axis=0)
-        f["train_labels"][-5:] = torch.zeros(5)
-
-    # Get all image file names in the directory for malignant
-    train_malignant_files = [
-        file for file in os.listdir(train_malignant_directory) if file.endswith(".jpg")
-    ]
-
-    # Create an h5 file to store the training tensors
-
-    for file in tqdm.tqdm(
-        train_malignant_files, desc="Processing malignant images", leave=False
-    ):
-        image = torchvision.io.read_image(
-            os.path.join(train_malignant_directory, file),
-            torchvision.io.image.ImageReadMode.RGB,
-        )
-        image30 = transform30(image)
-        image45 = trandform45(image)
-        image60 = transform60(image)
-        image90 = transform90(image)
-        image0 = transform0(image)
-
-        # Resize the dataset to add 5 more images and labels
-        f["train"].resize(f["train"].shape[0] + 5, axis=0)
-        f["train"][-5:] = torch.stack([image30, image45, image60, image90, image0])
-        f["train_labels"].resize(f["train_labels"].shape[0] + 5, axis=0)
-        f["train_labels"][-5:] = torch.ones(5)
+        f["train_labels"][-5:] = torch.tensor([label] * 5)
 
     ########################### Test data preprocessing ###########################
 
+    print("\nProcessing the test data...\n")
     # Get benign file names in the test directory
     test_benign_files = [
-        file for file in os.listdir(test_benign_directory) if file.endswith(".jpg")
+        (file, 0) for file in os.listdir(test_benign_directory) if file.endswith(".jpg")
     ]
-
-    f.create_dataset("test", (0, 3, 224, 224), maxshape=(None, 3, 224, 224))
-    f.create_dataset("test_labels", (0,), maxshape=(None,))
-    for file in tqdm.tqdm(
-        test_benign_files, desc="Processing benign images", leave=False
-    ):
-        image = torchvision.io.read_image(
-            os.path.join(test_benign_directory, file),
-            torchvision.io.image.ImageReadMode.RGB,
-        )
-
-        image0 = transform0(image)
-
-        # Resize the dataset to add 1 more image and label
-        f["test"].resize(f["test"].shape[0] + 1, axis=0)
-        f["test"][-1:] = torch.stack([image0])
-        f["test_labels"].resize(f["test_labels"].shape[0] + 1, axis=0)
-        f["test_labels"][-1:] = torch.zeros(1)
 
     # Get malignant file names in the test directory
     test_malignant_files = [
-        file for file in os.listdir(test_malignant_directory) if file.endswith(".jpg")
+        (file, 1)
+        for file in os.listdir(test_malignant_directory)
+        if file.endswith(".jpg")
     ]
 
-    # Create an h5 file to store the test tensors
+    # Concatenate the benign and malignant file names
+    test_files = test_benign_files + test_malignant_files
 
-    for file in tqdm.tqdm(
-        test_malignant_files, desc="Processing malignant images", leave=False
+    # Shuffle the file names
+    random.shuffle(test_files)
+
+    f.create_dataset("test", (0, 3, 224, 224), maxshape=(None, 3, 224, 224))
+    f.create_dataset("test_labels", (0,), maxshape=(None,))
+    for file, label in tqdm.tqdm(
+        test_files, desc="Processing test images", leave=False
     ):
+        directory = test_benign_directory if label == 0 else test_malignant_directory
         image = torchvision.io.read_image(
-            os.path.join(test_malignant_directory, file),
+            os.path.join(directory, file),
             torchvision.io.image.ImageReadMode.RGB,
         )
+
         image0 = transform0(image)
 
         # Resize the dataset to add 1 more image and label
         f["test"].resize(f["test"].shape[0] + 1, axis=0)
         f["test"][-1:] = torch.stack([image0])
         f["test_labels"].resize(f["test_labels"].shape[0] + 1, axis=0)
-        f["test_labels"][-1:] = torch.ones(1)
+        f["test_labels"][-1:] = torch.tensor([label])
+
+
+print("Data preprocessing is complete.")
